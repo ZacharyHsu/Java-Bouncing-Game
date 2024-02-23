9896e4290b505de3420a8d73454d419c2c784ff3
ZacharyHsu <zachary14584@gmail.com>
2024-02-23 20:42:23 +0800

    commit java bouncing game to github

---
+++ ShapeType.java
@@ -0,0 +1,19 @@
+/*
+ *  ============================================================================================
+ *  enum which defines the type of shapes in A1
+ *  YOUR UPI: 791039156
+ *  Name: Zachary Hsu
+ *  ============================================================================================
+ */
+import java.util.*;
+enum ShapeType { RECTANGLE, OVAL, NESTED;
+	private static final Random rand = new Random(30);
+	private static final int SIZE = values().length;
+	public static final ShapeType getShapeType(int index) { return values()[index]; }
+	public ShapeType next() {
+		return values()[(ordinal() + 1) % values().length];
+		}
+	public static ShapeType getRandomShapeType()  {
+	    return values()[rand.nextInt(SIZE)];
+  }
+}
